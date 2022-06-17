a6c1b4d52a2798f852ee193b3d2edc69b89e2b94
tarique vieira ramos <tariquevieira@gmail.com>
2022-06-16 21:13:48 -0300

    refactor: pratos

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -4,6 +4,7 @@ import classNames from 'classnames';
 import cardapio from 'data/cardapio.json';
 import NotFound from 'pages/NotFound';
 import PaginaPadrao from 'components/PaginaPadrao';
+import TagsPrato from 'components/TagsPrato';
 
 export default function Prato() {
   const { id } = useParams();
@@ -25,24 +26,7 @@ export default function Prato() {
         </div>
         <div className={styles.conteudo}>
           <p className={styles.conteudo__descricao}>{prato.description}</p>
-          <div className={styles.tags}>
-            <div
-              className={classNames({
-                [styles.tags__tipo]: true,
-                [styles[`tags__tipo__${prato.category.label.toLowerCase()}`]]:
-                  true,
-              })}
-            >
-              {prato.category.label}
-            </div>
-            <div className={styles.tags__porcao}>{prato.size}g</div>
-            <div className={styles.tags__qtdpessoas}>
-              Serve {prato.serving} pessoa {prato.serving == 1 ? '' : 's'}
-            </div>
-            <div className={styles.tags__valor}>
-              R$ {prato.price.toFixed(2)}
-            </div>
-          </div>
+          <TagsPrato {...prato} />
         </div>
       </section>
     </PaginaPadrao>
